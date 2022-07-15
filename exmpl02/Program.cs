aa14b93071d6abbb607c00e93706cb73b04b823a
kostalev <arctic@list.ru>
2022-07-15 19:04:23 +0500

    решение Задачи 2

---
+++ exmpl02/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = new Random().Next();
+int b = new Random().Next();
+int c = new Random().Next();
+
+Console.WriteLine("a = {0}, b = {1}, c = {2}", a, b, c);
+
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+
+Console.Write("max = {0}", max);
